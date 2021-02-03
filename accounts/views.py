d18b30ba7bc375898c48e4813e6132c00bd98551
jety54217 <jeyu54217@gmail.com>
2021-02-04 00:07:35 +0800

    1st version

---
+++ accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def accounts_view(request):
+    return HttpResponse("Your Account")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
